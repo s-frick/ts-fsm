e38cbe347b71e1d61a8f0adb71aef650bbb92263
Sebastian Frick <sebastian@monkeysquad.de>
2025-05-06 23:08:52 +0200

    feat: Add "Any" State

--- src/fsm.ts
+++ src/fsm.ts
@@ -1,24 +1,43 @@
 type State = number | number[];
 
-type Transition = number;
+type Transition = {
+  from: State;
+  to: State;
+};
 
-type FSM = {
-
-}
 function createState(state: State): number {
-  if (!state) throw new Error('State is required');
+  if (
+    state === null ||
+    state === undefined ||
+    (typeof state === "number" && state < 0)
+  )
+    throw new Error("State is required");
   if (Array.isArray(state)) {
     let s = 0;
     for (let i = 0; i < state.length; i++) {
-      s = (s | state[i]);
+      s = s | state[i];
     }
     return s;
   }
   return state;
 }
 
-export function createFSM(states: State[], initial: State) {
-  if (!initial) throw new Error('Initial state is required');
+type Event = number;
+type FSM = {
+  matches: (s: State) => boolean;
+  send: (e: Event) => void;
+};
+type FSMDefinition = {
+  initial: State;
+  states: State[];
+  transitions: Transition[];
+};
+export function createFSM({
+  initial,
+  states,
+  transitions,
+}: FSMDefinition): FSM {
+  if (!initial) throw new Error("Initial state is required");
   let currentState = createState(initial);
 
   const fsm: number[] = [];
@@ -26,22 +45,24 @@ export function createFSM(states: State[], initial: State) {
     const state = createState(states.pop()!);
     fsm.push(state);
   }
-  console.log(`States: [`)
+  console.log(`States: [`);
   for (const state of fsm) {
-    console.log(`  ${state.toString(2).padStart(8, '0')},`)
+    console.log(`  ${state.toString(2).padStart(8, "0")},`);
   }
-  console.log(`]`)
+  console.log(`]`);
 
   function matches(s: State) {
     const mask = createState(s);
-    console.log(`State: ${currentState.toString(2).padStart(8, '0')}`);
-    console.log(` mask: ${mask.toString(2).padStart(8, '0')}`);
+    console.log(`State: ${currentState.toString(2).padStart(8, "0")}`);
+    console.log(` mask: ${mask.toString(2).padStart(8, "0")}`);
     return (currentState & mask) === mask;
   }
-  function send() {}
+
+  function send(e: Event) {}
 
   return {
     matches,
-    send
-  }
+    send,
+  };
 }
+export const Any = 0;
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { createFSM } from "./fsm.js";
+import { createFSM, Any } from "./fsm.js";
 
 const Red = 1;
 const Yellow = 1 << 1;
@@ -10,8 +10,14 @@ const Solid = 1 << 4;
 const Fast = 1 << 5;
 const Slow = 1 << 6;
 
-const machine = createFSM(
-  [
+const TrafficLightFailure = {
+  from: Any,
+  to: [Yellow, Blinking, Fast],
+};
+
+const machine = createFSM({
+  initial: [Yellow, Blinking, Fast],
+  states: [
     Red,
     [Red, Blinking, Fast],
     [Yellow, Solid],
@@ -19,9 +25,10 @@ const machine = createFSM(
     [Yellow, Blinking, Slow],
     Green,
   ],
-  [Yellow, Blinking, Fast],
-);
+  transitions: [TrafficLightFailure],
+});
 
+console.log("Any matches any state", machine.matches(Any)); // true
 console.log(machine.matches(Yellow | Blinking | Fast)); // true
 console.log(machine.matches(Yellow)); // true
 console.log(machine.matches(Yellow | Solid | Fast)); // false
